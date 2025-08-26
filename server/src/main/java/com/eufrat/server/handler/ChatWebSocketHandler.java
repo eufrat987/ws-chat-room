d0a20a494e921c74f83b1401c2d7f5576caef63d
Eufrat <lukzyw@gmail.com>
2025-08-26 15:03:08 +0000

    Basic websocket connect via spring

--- server/src/main/java/com/eufrat/server/handler/ChatWebSocketHandler.java
+++ server/src/main/java/com/eufrat/server/handler/ChatWebSocketHandler.java
@@ -7,6 +7,22 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.TextMessage;
+
+import java.io.IOException;
+import java.lang.InterruptedException;
 
 public class ChatWebSocketHandler extends TextWebSocketHandler {
+
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message) 
+		throws InterruptedException, IOException {
+		System.out.println("hello all");
+		System.out.println(message.getPayload());
+	}
+
+	//@Override
+	//public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+	//}
 }
